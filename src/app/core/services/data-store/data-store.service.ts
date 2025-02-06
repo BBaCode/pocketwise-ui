ed50c5ee92de04ac26e32fd1416dc66ec542bd53
Brian Bassett <brianbsstt@gmail.com>
2025-02-06 11:54:36 -0500

    8080 to 80

--- src/app/core/services/data-store/data-store.service.ts
+++ src/app/core/services/data-store/data-store.service.ts
@@ -20,7 +20,7 @@ export class DataStoreService {
   dataStore: BehaviorSubject<DataStore>;
 
   private useMockData = false; // Toggle this to use mock data
-  private apiUrl = `http://pocketwise-server-production.up.railway.app:8080`; // prod
+  private apiUrl = `http://pocketwise-server-production.up.railway.app:80`; // prod
   // private apiUrl = 'http://localhost:8080'; // local
   private store: {
     accounts: Array<Account> | null;
